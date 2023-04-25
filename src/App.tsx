dc384500f719bb235e58c98278696bad4ac1d0a0
Harrison <Harrisontbusby@gmail.com>
2023-04-25 11:19:11 -0700

    Login Page Framework Progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import LoginPage from './Components/LoginPage/LoginPage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
    <>
-   <div>funyon</div>
+   <LoginPage/>
    
    </>
   );
--- src/Components/LoginPage/LoginPage.tsx
+++ src/Components/LoginPage/LoginPage.tsx
@@ -1,8 +1,59 @@
-import React from 'react';
-import './LoginPage.css';
+import React from "react";
+import { Row, Form } from "react-bootstrap";
+import "./LoginPage.css";
 
 export default function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <>
+            <div className="backgroundColorPage">
+                <Row>
+                    <div className="d-flex justify-content-center">
+                        <h1 className="headerTitle fontShadow">Task Master</h1>
+                    </div>
+                    <div className="d-flex justify-content-center mt-5">
+                        <Row className="loginBox">
+
+                            {/* TitleLogin */}
+                            <div className="d-flex justify-content-center mt-5">
+                                <h1 className="fontShadow loginTitle">Login</h1>
+                            </div>
+
+                            {/* Username Input */}
+                            <div className="mt-5 d-flex justify-content-center">
+                                <input
+                                    type="text"
+                                    placeholder="Username"
+                                    className="inputFont"
+                                />
+                            </div>
+                            {/* Password Input */}
+                            <div className="mt-5 d-flex justify-content-center">
+                            <input
+                                    type="text"
+                                    placeholder="Password"
+                                    className="inputFont"
+                                />
+
+                            </div>
+
+                            {/* Login Button */}
+                            <div>
+
+                            </div>
+
+                            {/* SignUp button */}
+                            <div className="mt-5 my-1 d-flex justify-content-center">
+                                <button className="loginBtn"><u>Login</u></button>
+                            </div>
+
+                            {/* Create Account Button */}
+                            <div className="my-5 d-flex justify-content-end">
+                                <button className="createAccountBtn"><u>Create Account?</u></button>
+                            </div>
+                        </Row>
+                    </div>
+                </Row>
+            </div>
+        </>
+    );
 }
