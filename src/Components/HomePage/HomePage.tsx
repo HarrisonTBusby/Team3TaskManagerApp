b61333f6c013643696bab536ab8dc7318d160b1c
Harrison <Harrisontbusby@gmail.com>
2023-04-26 11:47:09 -0700

    Home page continuation, made task boxes

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,29 +1,42 @@
-import React from 'react'
-import { Row, Col, Container } from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './HomePage.css';
+import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./HomePage.css";
 
 export default function HomePage() {
   return (
     <body>
-      <div className='homePageBackground'>
-
+      <div className="homePageBackground">
         <Container>
-        <Row className='d-flex justify-content-between'>
-
-        <div style={{ display: 'flex', alignItems: 'center' }}>
-          <div className='headerFontSize'>Task Master</div>
-          <button className='signOutBtn'><u>Sign out</u></button>
-        </div>
-        </Row>
+          <Row>
+            <div className="d-flex justify-content-center">
+              <div className="headerFontSize">Task Master</div>
+              <button className="signOutBtn">
+                <u>Sign out</u>
+              </button>
+            </div>
+          </Row>
         </Container>
 
-        <Container>
-        <Row className="mt-5">
-          <div className='userTaskFontSize'>[Placeholder] has 1 Task In Progress</div>
-        </Row>
+        <Container className="mt-2 d-flex justify-content-center">
+          <Row>
+            <div className="userTaskFontSize">
+              [Placeholder] has 1 Task In Progress
+            </div>
+          </Row>
         </Container>
       </div>
+
+      <Container className="taskContainer">
+        <Row className="d-flex justify-content-around">
+          <div className="toDoText">To Do:</div>
+          <div className="taskBox"></div>
+          <div className="inProgressText">In Progress:</div>
+          <div className="taskBox"></div>
+          <div className="completedText">Complete:</div>
+          <div className="taskBox"></div>
+        </Row>
+      </Container>
     </body>
-  )
+  );
 }
