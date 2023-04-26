e186dce2a9c0eb293632cbef818613f1fe941f54
Harrison <Harrisontbusby@gmail.com>
2023-04-26 11:59:48 -0700

    Card Componet 90% complete

--- src/Components/Card/CardComponent.tsx
+++ src/Components/Card/CardComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap';
+import { Card, Row } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./CardComponent.css"
 
@@ -7,14 +7,18 @@ export default function CardComponent() {
   return (
     <div className=''>
     <Card style={{ width: '30vw' }} className='cardFont'>
-      <Card.Body>
-        <Card.Title className='d-flex justify-content-center' style={{backgroundColor: 'green'}}>Task Title</Card.Title>
-        <div>
-        <p className="">Due: date   Priority:</p>
+      <Card.Body style={{padding: '0px'}}>
+        <Card.Title className='d-flex justify-content-center' style={{backgroundColor: '#23806D'}}>Task Title</Card.Title>
+        <div className='d-flex justify-content-around'>
+        <p className="">Due: date</p>
+        <p className="">Priority: Low</p>
         </div>
-        <Card.Text style={{backgroundColor: 'green'}}>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
+    <div className='d-flex justify-content-center'>
+
+        <p className="">Assigned By: Jacob Dekok</p>
+    </div>
+        <Card.Text style={{backgroundColor: '#23806D'}}>
+        We are working on a app that is designed to track tasks. These tasks will have: a title, description, due date, the admin who assigned it, and priority of the task. A proto...
         </Card.Text>
       </Card.Body>
     </Card>
