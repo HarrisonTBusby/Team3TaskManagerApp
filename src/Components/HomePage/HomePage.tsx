2f566f0b72dcfd19f935751e1533763fe71d7567
Harrison <Harrisontbusby@gmail.com>
2023-04-26 10:27:33 -0700

    Co-authored-by: Griffin <GriffinSwain@users.noreply.github.com>

--- src/Components/HomePage/HomePage.tsx
+++ src/Components/HomePage/HomePage.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { Row, Col, Container } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './HomePage.css';
 
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <body>
+      <div className='homePageBackground'>
+
+        <Container>
+        <Row className='d-flex justify-content-between'>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <div className='headerFontSize'>Task Master</div>
+          <button className='signOutBtn'><u>Sign out</u></button>
+        </div>
+        </Row>
+        </Container>
+
+        <Container>
+        <Row className="mt-5">
+          <div className='userTaskFontSize'>Placeholder has 1 Task In Progress</div>
+        </Row>
+        </Container>
+      </div>
+    </body>
   )
 }
