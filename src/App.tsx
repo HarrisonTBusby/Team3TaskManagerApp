5121491c495d2ff770d918e06330f006b486462b
MarkRam2 <116022322+MarkRam2@users.noreply.github.com>
2023-04-26 09:36:32 -0700

    progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Components/SignUpPage/SignUpPage'
+import SignUpPage from './Components/SignUpPage/SignUpPage';
 
 function App() {
   return (
    <>
-   <div>funyon</div>
+   <SignUpPage></SignUpPage>
    
    </>
   );
--- src/Components/SignUpPage/SignUpPage.tsx
+++ src/Components/SignUpPage/SignUpPage.tsx
@@ -1,8 +1,40 @@
-import React from 'react'
-import './SignUpPage.css';
+import React from "react";
+import { Row, Col, Button} from "react-bootstrap";
+import './HomePage.css';
+
 
 export default function SignUpPage() {
+
   return (
-    <div>SignUpPage</div>
-  )
+    <>
+      <div className="backgroundColorPage">
+        <Row>
+          <Col>
+            <div className="">
+              <h1 className="headerTitle fontShadow">Task Master</h1>
+            </div>
+            <div className="">
+              <h1 className="">Sign Up</h1>
+            </div>
+            <div className="">
+              <input></input>
+            </div>
+            <div className="">
+              <input></input>
+            </div>
+            <div className="">
+              <input></input>
+            </div>
+            <div className="">
+              <Button></Button>
+              <p>Admin Account</p>
+            </div>
+            <div className="">
+              <Button>Create Account</Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </>
+  );
 }
