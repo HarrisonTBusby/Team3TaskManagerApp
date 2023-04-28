f3195f4cbb2a3dbcbff169731f994d09d32c476e
Griffin <116022056+GriffinSwain@users.noreply.github.com>
2023-04-28 10:17:05 -0700

    Working on Create Task Modal

--- src/Components/AdminHomePage/AdminHomePage.tsx
+++ src/Components/AdminHomePage/AdminHomePage.tsx
@@ -3,18 +3,24 @@ import { Row, Col, Container, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './AdminHomePage.css';
 import CardComponent from '../Card/CardComponent';
+import CreateTaskModal from '../ModalComponent/CreateTaskModal/CreateTaskComponent';
 
 export default function AdminHomePage() {
+
+  function CreateTask() {
+    <CreateTaskModal/>
+  }
+
   return (
     <body>
     <div className='homePageBackground'>
-
+    <CreateTaskModal/>
       <Container>
       <Row className='d-flex justify-content-between'>
 
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <div className='headerFontSize'>Task Master</div>
-        <button className='createTasksBtn'><u>Create Tasks</u></button>
+        <button className='createTasksBtn' onClick={CreateTask}><u>Create Tasks</u></button>
         <button className='userBtn'><u>Users</u></button>
         <button className='settingsBtn'><u>Settings</u></button>
         <button className='signOutBtn'><u>Sign out</u></button>
--- src/Components/ModalComponent/CreateTaskModal/CreateTaskComponent.tsx
+++ src/Components/ModalComponent/CreateTaskModal/CreateTaskComponent.tsx
@@ -1,52 +1,72 @@
 import React from 'react'
-import { Row, Col, Modal, Button, Container } from 'react-bootstrap';
-import './ModalStyle.css'
+import { Row, Col, Modal, Button, Container, Dropdown } from 'react-bootstrap';
+import './CreateTaskStyle.css'
+import { useState } from 'react';
 
-export default function AdminModalComponent() {
+export default function CreateTaskModal() {
+
+  const [priority, setPriority] = useState('');
+
+  function highPriority(){
+    setPriority("High");
+  }
+  function mediumPriority(){
+    setPriority("Medium");
+  }
+  function lowPriority(){
+    setPriority("Low");
+  }
+
   return (
     <div
       className="modal show"
       style={{ display: 'block', position: 'initial' }}
     >
 
-    <Container className='Max'>
-      <Modal.Dialog>
+      <Container className='Max'>
+        <Modal.Dialog>
 
-        <Modal.Header className='title-cente' closeButton>
-          <Modal.Title>Modal title</Modal.Title>
-        </Modal.Header>
+          <Modal.Header className='title-cente' closeButton>
+            <Row>
+              <Col><Modal.Title>Task Name</Modal.Title></Col>
+              <Col><input type='text' className='nameInput'></input></Col>
+            </Row>
+          </Modal.Header>
 
-        <Row>
-          <Col className='basicInfoLeft '>
-            <p>Assigned to: [Admin User]</p>
-            <p>Due: 19/23/28</p>
-          </Col>
+          <Row className='basicInfoLeft'>
+            <Col><h4>Due Date</h4></Col>
+            <Col><input type='date' className='dateInput'></input></Col>
+            <Col><h4>Priority</h4></Col>
+            <Col><Dropdown>
+              <Dropdown.Toggle variant="danger" id="dropdown-basic">{priority}</Dropdown.Toggle>
 
-          <Col  className='basicInfo '>
-            <p>Assigned by: [Admin User]</p>
-            <p>Priority: medium</p>
-          </Col>
-        </Row>
+              <Dropdown.Menu>
+                <Dropdown.Item href="#/action-1">High</Dropdown.Item>
+                <Dropdown.Item href="#/action-2" onClick={highPriority}>Medium</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Low</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown></Col>
+          </Row>
 
-      <div className='headerline'>
-        <Modal.Header></Modal.Header>
-      </div>
+          <div className='headerline'>
+            <Modal.Header></Modal.Header>
+          </div>
 
 
-        <Modal.Body className='assign-center'>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae voluptate consectetur eaque, quisquam tenetur ex quod nesciunt aperiam qui voluptatem nulla quibusdam ea at, illum culpa dolorem sunt!</p>
-        </Modal.Body>
+          <Modal.Body className='assign-center'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita ipsum vitae voluptate consectetur eaque, quisquam tenetur ex quod nesciunt aperiam qui voluptatem nulla quibusdam ea at, illum culpa dolorem sunt!</p>
+          </Modal.Body>
 
-        <Row>
-          <Col className='edit-Button'>
-            <Button className='btn-danger'>Delete</Button>
-            <p className=''>.</p>
-            <Button className='btn-success'>Edit</Button>
-          </Col>
-        </Row>
+          <Row>
+            <Col className='edit-Button'>
+              <Button className='btn-danger'>Delete</Button>
+              <p className=''>.</p>
+              <Button className='btn-success'>Edit</Button>
+            </Col>
+          </Row>
 
-      </Modal.Dialog>
-    </Container>
+        </Modal.Dialog>
+      </Container>
     </div>
   )
 }
